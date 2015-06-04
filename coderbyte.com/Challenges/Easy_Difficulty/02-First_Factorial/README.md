34d3c5a87b9fdfc95e041b726be0404b8def2df5
holblin <holblin@gmail.com>
2015-06-04 23:03:42 +0200

    Add coderbyte.com/challenge/easy/2

---
+++ coderbyte.com/Challenges/Easy_Difficulty/02-First_Factorial/README.md
@@ -0,0 +1,7 @@
+# First Factorial
+
+## Question
+Using the JavaScript language, have the function `FirstFactorial(num)` take the `num` parameter being passed and return the factorial of it.
+(<b>ie.</b> if `num = 4, return (4 * 3 * 2 * 1)`).
+For the test cases, the range will be between 1 and 18.
+Use the <b>Parameter Testing</b> feature in the box below to test your code with different arguments.
---
+++ coderbyte.com/Challenges/Easy_Difficulty/02-First_Factorial/solution.js
@@ -0,0 +1,21 @@
+// non recursive solution
+function FirstFactorial(num) { 
+  res = 1;
+  for(var i = 2; i <= num; i ++) {
+    res *= i;
+  }
+  return res;
+}
+
+// recursive solution
+function FirstFactorial_2(num) {
+  if(num < 2){
+    return 1;
+  }
+  
+  return num * FirstFactorial(num -1);
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstFactorial(readline());           
